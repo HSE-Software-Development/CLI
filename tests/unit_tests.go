4c5ff15200fce798211d28bbcfbaedc5c35f9bb4
Pushin Maxim <macsime12321@gmail.com>
2025-03-16 22:15:44 +0300

    add unit-test for pwd

---
+++ tests/unit_tests.go
@@ -0,0 +1,21 @@
+package unit_tests
+
+import (
+	"CLI/internal/executor"
+	"testing"
+	"bytes"
+	"os"
+)
+
+func TestPwdCommand(t *testing.T) {
+	executor := executor.NewExecutor()
+	input := bytes.NewBufferString("")
+	output, err := executor.Execute("pwd", input)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	expected, _ := os.Getwd()
+	if output.String() != expected+"\n" {
+		t.Errorf("Expected '%s', got '%s'", expected, output.String())
+	}
+}
